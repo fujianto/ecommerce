bd72c72cbb5de78be9cf0a66952116db44460719
Septian Ahmad Fujianto <septianahmad@naisinpo.com>
2017-11-22 17:12:56 +0700

    Prepare for production env

--- server/app.js
+++ server/app.js
@@ -1,21 +1,57 @@
-// const express = require('express');
-const logger = require('morgan');
-const bodyParser = require('body-parser');
-const app = require('express') ();
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+const index = require('./routes/index');
 const customerRouter = require('./routes/customerRoute')
 const productRouter = require('./routes/productRoute');
 const transactionRouter = require('./routes/transactionRoute');
+
+var app = express();
 const cors = require('cors')
 
+// const url = 'mongodb://localhost:27017/ecommerce';
+const url = 'mongodb://admin:admin@mongocrud-shard-00-00-ilsdt.mongodb.net:27017,mongocrud-shard-00-01-ilsdt.mongodb.net:27017,mongocrud-shard-00-02-ilsdt.mongodb.net:27017/ecommerce_db?ssl=true&replicaSet=mongoCrud-shard-0&authSource=admin';
+const mongoose = require('mongoose');
+mongoose.connect(url);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', index);
 app.use('/products', productRouter);
 app.use('/customers', customerRouter);
 app.use('/transactions', transactionRouter);
 
-app.listen(3000);
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
