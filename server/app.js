371e9139ad351553621c964f5d28c1aaf920eece
Septian Ahmad Fujianto <septianahmad@naisinpo.com>
2017-11-22 20:29:45 +0700

    Merge from master

--- server/app.js
+++ server/app.js
@@ -13,8 +13,10 @@ const transactionRouter = require('./routes/transactionRoute');
 var app = express();
 const cors = require('cors')
 
-// const url = 'mongodb://localhost:27017/ecommerce';
-const url = 'mongodb://admin:admin@mongocrud-shard-00-00-ilsdt.mongodb.net:27017,mongocrud-shard-00-01-ilsdt.mongodb.net:27017,mongocrud-shard-00-02-ilsdt.mongodb.net:27017/ecommerce_db?ssl=true&replicaSet=mongoCrud-shard-0&authSource=admin';
+// Productions
+// const url = 'mongodb://admin:admin@mongocrud-shard-00-00-ilsdt.mongodb.net:27017,mongocrud-shard-00-01-ilsdt.mongodb.net:27017,mongocrud-shard-00-02-ilsdt.mongodb.net:27017/ecommerce_db?ssl=true&replicaSet=mongoCrud-shard-0&authSource=admin';
+
+const url = 'mongodb://localhost:27017/ecommerce';
 const mongoose = require('mongoose');
 mongoose.connect(url);
 
